3752af9ef8ddc5541678d34a174175646f65b726
Collins Mutai <collinsfrontend@gmail.com>
2022-07-29 18:53:12 +0300

    added update and complete

--- dist/script.js
+++ dist/script.js
@@ -5,7 +5,7 @@ const dateInput = document.getElementById("date");
 const submit = document.getElementById("addBtn");
 const form = document.getElementById("form");
 const tasksList = document.getElementById("tasksList");
-const editBtn = document.getElementById("editBtn");
+const editBtns = document.getElementById("editBtns");
 const addBtn = document.getElementById("addBtn");
 class IncompletedTodos {
     constructor() {
@@ -16,20 +16,17 @@ class IncompletedTodos {
             this.display();
         };
         this.updateTask = (id) => {
-            const item = this.getId(id);
-            titleInput.value = item.title;
-            descriptionInput.value = item.description;
-            dateInput.value = item.date;
-            console.log(titleInput.value);
-            // this.todos.splice(id, 1);
-            // this.display();
+            this.todos[id] = {
+                title: titleInput.value,
+                description: descriptionInput.value,
+                date: dateInput.value,
+            };
+            task.display();
         };
         this.display();
     }
     addTodo(item) {
-        // console.log(item);
         this.todos.push(item);
-        // form.reset()
     }
     getAllTodos() {
         form.reset();
@@ -39,21 +36,26 @@ class IncompletedTodos {
         return this.todos.find((element, index) => index === id);
     }
     display() {
-        tasksList.innerHTML = ' ';
+        tasksList.innerHTML = " ";
         this.getAllTodos().map((task, index) => {
-            const mainDiv = document.createElement('div');
-            mainDiv.className = 'divLists';
-            const check = document.createElement('input');
-            check.type = 'checkbox';
-            const p1 = document.createElement('p');
-            const p2 = document.createElement('p');
-            const p3 = document.createElement('p');
-            const editButton = document.createElement('button');
-            const deleteButton = document.createElement('button');
-            editButton.id = 'editBtn';
-            editButton.innerHTML = 'Edit';
-            deleteButton.id = 'deleteBtn';
-            deleteButton.innerHTML = 'Delete';
+            const mainDiv = document.createElement("div");
+            mainDiv.className = "divLists";
+            const check = document.createElement("input");
+            check.type = "checkbox";
+            check.checked = false;
+            // check.addEventListener('click',()=>{
+            //   let completelist = document.querySelector('#complete')
+            //   completelist?.appendChild(mainDiv);
+            // })
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+            const p3 = document.createElement("p");
+            const editButton = document.createElement("button");
+            const deleteButton = document.createElement("button");
+            editButton.id = "editBtn";
+            editButton.innerHTML = "Edit";
+            deleteButton.id = "deleteBtn";
+            deleteButton.innerHTML = "Delete";
             p1.textContent = `${task.title}`;
             p2.textContent = `${task.description}`;
             p3.textContent = `${task.date}`;
@@ -64,29 +66,48 @@ class IncompletedTodos {
             mainDiv.appendChild(editButton);
             mainDiv.appendChild(deleteButton);
             tasksList.appendChild(mainDiv);
-            deleteButton.addEventListener('click', () => {
+            deleteButton.addEventListener("click", () => {
                 deleteLi(index);
             });
-            editButton.addEventListener('click', () => {
-                // console.log(index)
+            editButton.addEventListener("click", () => {
+                titleInput.value = task.title;
+                descriptionInput.value = task.description;
+                dateInput.value = task.date;
+                // console.log(titleInput.value);
                 editLi(index);
+                editBtns.style.display = "block";
+                addBtn.style.display = "none";
+            });
+            check.addEventListener("click", () => {
+                checkLi(index);
             });
         });
     }
 }
-function editLi(index) {
-    // console.log(index);
-    // const editVar = new 
-    task.updateTask(index);
+class CompletedTodos extends IncompletedTodos {
+    constructor() {
+        super();
+        this.completedArray = [];
+    }
+    completeTodo(items) {
+        this.completedArray.push(items);
+    }
+    getAllTodos() {
+        form.reset();
+        return this.completedArray;
+    }
+    checkedItems() {
+        console.log("checked todo");
+    }
 }
-// editBtn.addEventListener('click', () => {
-// })
 const task = new IncompletedTodos();
-submit.addEventListener('click', (e) => {
+const completetask = new IncompletedTodos();
+submit.addEventListener("click", (e) => {
     e.preventDefault();
     const title = titleInput.value;
     const description = descriptionInput.value;
     const date = dateInput.value;
+    const check = false;
     if (titleInput.value === "") {
         alert("Title is required");
         return;
@@ -109,71 +130,35 @@ function deleteLi(index) {
     task.deleteTask(index);
     task.getAllTodos();
 }
-// const todoList: TodoItem[] = [];
-// addBtn?.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (title.value === "") {
-//     alert("Title is required");
-//     return;
-//   }
-//   if (description.value === "") {
-//     alert("Description is required");
-//     return;
-//   }
-//   if (date.value === "") {
-//     alert("Date is required");
-//     return;
-//   }
-//   let newTodo: TodoItem = {
-//     title: title?.value,
-//     description: description?.value,
-//     date: date?.value,
-//     completed:false
-//   };
-//   todoList.push(newTodo);
-//   console.log(todoList);
-//   form.reset();
-//   const c = new PaintHtmlToDom(todoList, tasksList);
-//   c.html();
-// });
-// class PaintHtmlToDom {
-//   constructor(public data: any, id: any) { }
-//   html() {
-//     const htmlData = this.data
-//       .map((item: any, index: number) => {
-//         let htmlCode = `
-//         <div class="todo-items">
-//         <ul style="list-style: none;">
-//             <li id="taskDone">
-//                 <span  id="checkmark" style="padding-right: 10px;">
-//                   <input ${
-//                     this.data.completed ? "checked" : ""
-//                   } onchange="completeTask(this, ${index})"type="checkbox" />
-//                 </span>
-//                 <span style="padding-right: 10px;" id="titleText">${item.title}</span>
-//                 <span id="descriptionText">${item.description}</span>
-//                 <span id="dateText">${item.date}</span>
-//                 <span id="editBtn" style="padding-left: 10px;">
-//                 <button id="editBtn" onclick="editTask(${index})" type="button">Edit</button>
-//                 </span><span><button onclick="this.deleteTask(${index})" id="deleteBtn" type="button">Delete </button>
-//                 </span>
-//             </li>
-//         </ul>
-//     </div>
-//     `;
-//         return htmlCode;
-//       })
-//       .join("");
-//     tasksList.innerHTML = htmlData;
-//   }
-//   getId(id:any){
-//       return todoList.find((element, index) => index === id);
-//   }
-//   deleteTask = (id:any) =>{
-//       const item:any = this.getId(id);
-//       console.log(item);
-//       todoList.splice(item, 1);
-//       console.log(todoList);
-//       this.html();
-//   }
-// }
+function editLi(index) {
+    editBtns.addEventListener("click", (e) => {
+        e.preventDefault();
+        task.updateTask(index);
+        editBtns.style.display = "none";
+        addBtn.style.display = "block";
+    });
+}
+function checkLi(index) {
+    const singlecompletedtask = task.getAllTodos()[index];
+    // Add to completed Array
+    completetask.getAllTodos().push(Object.assign({}, singlecompletedtask));
+    console.log(completetask.getAllTodos());
+    // remove from task array
+    task.getAllTodos().splice(index, 1);
+    task.display();
+    let completed = document.querySelector("#complete");
+    completetask.getAllTodos().map(function (item, i) {
+        console.log(item.title);
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("p");
+        p1.textContent = item.title;
+        p2.textContent = item.description;
+        p3.textContent = item.date;
+        const newDiv = document.createElement("div");
+        newDiv.appendChild(p1);
+        newDiv.appendChild(p2);
+        newDiv.appendChild(p3);
+        completed.appendChild(newDiv);
+    });
+}
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,13 @@
 const titleInput = document.getElementById("title") as HTMLInputElement;
-const descriptionInput = document.getElementById("description") as HTMLInputElement;
+const descriptionInput = document.getElementById(
+  "description"
+) as HTMLInputElement;
 const dateInput = document.getElementById("date") as HTMLInputElement;
 const submit = document.getElementById("addBtn") as HTMLButtonElement;
 const form = document.getElementById("form") as HTMLFormElement;
 const tasksList = document.getElementById("tasksList") as HTMLDivElement;
-const editBtn = document.getElementById("editBtn") as HTMLButtonElement;
+const editBtns = document.getElementById("editBtns") as HTMLButtonElement;
 const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
-
 
 interface TodoItem {
   title: string;
@@ -17,101 +18,114 @@ interface TodoItem {
 class IncompletedTodos {
   private todos: TodoItem[] = [];
   constructor() {
-    this.display()
+    this.display();
   }
   addTodo(item: TodoItem) {
-    // console.log(item);
-    this.todos.push(item)
-    // form.reset()
+    this.todos.push(item);
   }
   getAllTodos() {
-    form.reset()
+    form.reset();
     return this.todos;
-
   }
   getId(id: any) {
     return this.todos.find((element, index) => index === id);
   }
   display() {
-    tasksList.innerHTML = ' '
+    tasksList.innerHTML = " ";
     this.getAllTodos().map((task: any, index: any) => {
-      const mainDiv = document.createElement('div')
-      mainDiv.className = 'divLists'
-      const check = document.createElement('input')
-      check.type = 'checkbox'
-      const p1 = document.createElement('p')
-      const p2 = document.createElement('p')
-      const p3 = document.createElement('p')
-      const editButton = document.createElement('button')
-      const deleteButton = document.createElement('button')
-      editButton.id = 'editBtn';
-      editButton.innerHTML = 'Edit'
-      deleteButton.id = 'deleteBtn'
-      deleteButton.innerHTML = 'Delete'
+      const mainDiv = document.createElement("div");
+      mainDiv.className = "divLists";
+
+      const check = document.createElement("input");
+      check.type = "checkbox";
+      check.checked = false;
+      // check.addEventListener('click',()=>{
+
+      //   let completelist = document.querySelector('#complete')
+      //   completelist?.appendChild(mainDiv);
+      // })
+
+      const p1 = document.createElement("p");
+      const p2 = document.createElement("p");
+      const p3 = document.createElement("p");
+      const editButton = document.createElement("button");
+      const deleteButton = document.createElement("button");
+      editButton.id = "editBtn";
+      editButton.innerHTML = "Edit";
+      deleteButton.id = "deleteBtn";
+      deleteButton.innerHTML = "Delete";
       p1.textContent = `${task.title}`;
       p2.textContent = `${task.description}`;
       p3.textContent = `${task.date}`;
-      mainDiv.appendChild(check)
+      mainDiv.appendChild(check);
       mainDiv.appendChild(p1);
       mainDiv.appendChild(p2);
       mainDiv.appendChild(p3);
-      mainDiv.appendChild(editButton)
-      mainDiv.appendChild(deleteButton)
+      mainDiv.appendChild(editButton);
+      mainDiv.appendChild(deleteButton);
       tasksList.appendChild(mainDiv);
 
-      deleteButton.addEventListener('click', () => {
-        deleteLi(index)
-      })
-      editButton.addEventListener('click', () => {
-        // console.log(index)
-        editLi(index)
-      })
-
-    })
+      deleteButton.addEventListener("click", () => {
+        deleteLi(index);
+      });
+      editButton.addEventListener("click", () => {
+        titleInput.value = task.title;
+        descriptionInput.value = task.description;
+        dateInput.value = task.date;
+        // console.log(titleInput.value);
+        editLi(index);
+        editBtns.style.display = "block";
+        addBtn.style.display = "none";
+      });
+      check.addEventListener("click", () => {
+        checkLi(index);
+      });
+    });
   }
+
   deleteTask = (id: number) => {
     const item: any = this.getId(id);
-
 
     this.todos.splice(id, 1);
 
     this.display();
-  }
+  };
   updateTask = (id: number) => {
-    const item: any = this.getId(id);
+    this.todos[id] = {
+      title: titleInput.value,
+      description: descriptionInput.value,
+      date: dateInput.value,
+    };
 
-    titleInput.value = item.title;
-    descriptionInput.value = item.description;
-    dateInput.value = item.date;
-
-    console.log(titleInput.value);
-
-
-
-    // this.todos.splice(id, 1);
-
-    // this.display();
+    task.display();
+  };
+}
+class CompletedTodos extends IncompletedTodos {
+  public completedArray: TodoItem[] = [];
+  constructor() {
+    super();
   }
-
+  completeTodo(items: TodoItem) {
+    this.completedArray.push(items);
+  }
+  getAllTodos() {
+    form.reset();
+    return this.completedArray;
+  }
+  checkedItems() {
+    console.log("checked todo");
+  }
 }
-function editLi(index: number) {
-  // console.log(index);
-
-  // const editVar = new 
-  task.updateTask(index)
-
-}
-// editBtn.addEventListener('click', () => {
-  
-// })
 
 const task = new IncompletedTodos();
+const completetask = new IncompletedTodos();
 
-submit.addEventListener('click', (e) => {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
   const title = titleInput.value;
   const description = descriptionInput.value;
   const date = dateInput.value;
+  const check = false;
   if (titleInput.value === "") {
     alert("Title is required");
     return;
@@ -132,106 +146,48 @@ submit.addEventListener('click', (e) => {
 });
 
 function deleteLi(index: any) {
-  task.deleteTask(index)
-  task.getAllTodos()
+  task.deleteTask(index);
+  task.getAllTodos();
+}
+function editLi(index: number) {
+  editBtns.addEventListener("click", (e) => {
+    e.preventDefault();
+    task.updateTask(index);
+    editBtns.style.display = "none";
+    addBtn.style.display = "block";
+  });
 }
 
+function checkLi(index: number) {
+  const singlecompletedtask = task.getAllTodos()[index];
 
+  // Add to completed Array
+  completetask.getAllTodos().push({ ...singlecompletedtask });
+  console.log(completetask.getAllTodos());
 
+  // remove from task array
 
+  task.getAllTodos().splice(index, 1);
+  task.display();
 
+  let completed = document.querySelector("#complete") as HTMLDivElement;
 
+  completetask.getAllTodos().map(function (item, i) {
+    console.log(item.title);
 
+    const p1 = document.createElement("p") as HTMLParagraphElement;
+    const p2 = document.createElement("p") as HTMLParagraphElement;
+    const p3 = document.createElement("p") as HTMLParagraphElement;
 
+    p1.textContent = item.title;
+    p2.textContent = item.description;
+    p3.textContent = item.date;
 
+    const newDiv = document.createElement("div");
+    newDiv.appendChild(p1);
+    newDiv.appendChild(p2);
+    newDiv.appendChild(p3);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const todoList: TodoItem[] = [];
-// addBtn?.addEventListener("click", (e) => {
-//   e.preventDefault();
-
-//   if (title.value === "") {
-//     alert("Title is required");
-//     return;
-//   }
-//   if (description.value === "") {
-//     alert("Description is required");
-//     return;
-//   }
-//   if (date.value === "") {
-//     alert("Date is required");
-//     return;
-//   }
-//   let newTodo: TodoItem = {
-//     title: title?.value,
-//     description: description?.value,
-//     date: date?.value,
-//     completed:false
-//   };
-//   todoList.push(newTodo);
-//   console.log(todoList);
-//   form.reset();
-//   const c = new PaintHtmlToDom(todoList, tasksList);
-//   c.html();
-// });
-
-// class PaintHtmlToDom {
-//   constructor(public data: any, id: any) { }
-//   html() {
-//     const htmlData = this.data
-//       .map((item: any, index: number) => {
-//         let htmlCode = `
-//         <div class="todo-items">
-//         <ul style="list-style: none;">
-//             <li id="taskDone">
-//                 <span  id="checkmark" style="padding-right: 10px;">
-//                   <input ${
-//                     this.data.completed ? "checked" : ""
-//                   } onchange="completeTask(this, ${index})"type="checkbox" />
-//                 </span>
-//                 <span style="padding-right: 10px;" id="titleText">${item.title}</span>
-//                 <span id="descriptionText">${item.description}</span>
-//                 <span id="dateText">${item.date}</span>
-//                 <span id="editBtn" style="padding-left: 10px;">
-//                 <button id="editBtn" onclick="editTask(${index})" type="button">Edit</button>
-//                 </span><span><button onclick="this.deleteTask(${index})" id="deleteBtn" type="button">Delete </button>
-//                 </span>
-//             </li>
-//         </ul>
-//     </div>
-//     `;
-//         return htmlCode;
-//       })
-//       .join("");
-//     tasksList.innerHTML = htmlData;
-//   }
-//   getId(id:any){
-//       return todoList.find((element, index) => index === id);
-//   }
-//   deleteTask = (id:any) =>{
-//       const item:any = this.getId(id);
-//       console.log(item);
-
-//       todoList.splice(item, 1);
-//       console.log(todoList);
-//       this.html();
-//   }
-// }
-
+    completed.appendChild(newDiv);
+  });
+}
